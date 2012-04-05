719a35811fb3c810371b347335e066fd4dc60ded
Dirkjan Ochtman <dirkjan@ochtman.nl>
2012-04-05 16:13:26 +0200

    Make the driver script slightly more usable.

--- lang.py
+++ lang.py
@@ -31,8 +31,21 @@ COMMANDS = {
 	'compile': compile,
 }
 
+def find(cmd):
+	if cmd in COMMANDS: return COMMANDS[cmd]
+	full = set(COMMANDS)
+	matched = sorted(i for i in COMMANDS if i.startswith(cmd))
+	if len(matched) == 1:
+		return COMMANDS[matched[0]]
+	elif len(matched) > 1:
+		print 'ambiguous command: %r' % cmd
+		return lambda x, y: None
+	else:
+		print 'no command found: %r' % cmd
+		return lambda x, y: None
+
 if __name__ == '__main__':
 	parser = optparse.OptionParser(description='the lang utility')
 	parser.add_option('--inline', help='inline stdlib', action='store_true')
 	opts, args = parser.parse_args()
-	COMMANDS[args[0]](args[1], opts)
+	find(args[0])(args[1], opts)
