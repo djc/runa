f229450d0616a78bb35503faea00e1a0a69e58ca
Dirkjan Ochtman <dirkjan@ochtman.nl>
2014-06-08 14:20:12 +0200

    Remove old parsing code from ast module

--- runac/ast.py
+++ runac/ast.py
@@ -24,720 +24,162 @@ class Expr(Node):
 		self.type = None
 		self.escapes = False
 
-# Terminals
-
-class Terminal(Node):
-	lbp = 0
-	def nud(self, p):
-		return self
-
-class End(Terminal):
-	pass
-
-class RightPar(Terminal):
-	op = ')'
-
-class ElemEnd(Terminal):
-	op = ']'
-
-class Colon(Terminal):
-	op = ':'
-
-class Comma(Terminal):
-	op = ','
-
-class RType(Terminal):
-	op = '->'
-
-class Indent(Terminal):
-	pass
-
-class Dedent(Terminal):
-	pass
-
-class NL(Terminal):
-	pass
-
-class Bool(Expr, Terminal):
+class Bool(Expr):
 	def __init__(self, val, pos):
 		Expr.__init__(self, pos)
 		self.val = True if val == 'True' else False
 
-class Int(Expr, Terminal):
+class Int(Expr):
 	def __init__(self, num, pos):
 		Expr.__init__(self, pos)
 		self.val = num
 
-class Float(Expr, Terminal):
+class Float(Expr):
 	def __init__(self, num, pos):
 		Expr.__init__(self, pos)
 		self.val = num
 
-class String(Expr, Terminal):
+class String(Expr):
 	def __init__(self, value, pos):
 		Expr.__init__(self, pos)
 		self.val = value
 
-class Name(Expr, Terminal):
+class Name(Expr):
 	def __init__(self, name, pos):
 		Expr.__init__(self, pos)
 		self.name = name
 
 # Expression-level
 
-class BinaryOp(Expr):
-	def led(self, p, left):
-		self.left = left
-		self.right = p.expr(self.lbp)
-		return self
-
-class Attrib(BinaryOp):
-	
-	op = '.'
-	lbp = 100
+class Attrib(Expr):
 	fields = 'obj',
-	
-	def led(self, p, left):
-		self.obj = left
-		self.attrib = p.advance(Name).name
-		return self
 
-class Slice(Expr):
-	fields = 'values',
-	def __init__(self, pos, vals):
-		self.values = vals
-
-class Elem(BinaryOp):
-	
-	op = '['
-	lbp = 16
+class Elem(Expr):
 	fields = 'obj', 'key'
-	
-	def led(self, p, left):
-		
-		values = []
-		while not isinstance(p.token, ElemEnd):
-			if isinstance(p.token, Colon):
-				values.append(None)
-				p.advance(Colon)
-			else:
-				values.append(p.expr())
-				if isinstance(p.token, Colon):
-					p.advance(Colon)
-					if isinstance(p.token, ElemEnd):
-						values.append(None)
-		
-		self.obj = left
-		if len(values) > 1:
-			self.key = Slice(left.pos, values)
-		else:
-			self.key = values[0]
-		
-		p.advance(ElemEnd)
-		return self
 
-class Add(BinaryOp):
-	op = '+'
-	lbp = 50
+class Add(Expr):
 	fields = 'left', 'right'
 
-class Sub(BinaryOp):
-	op = '-'
-	lbp = 50
+class Sub(Expr):
 	fields = 'left', 'right'
 
-class Mul(BinaryOp):
-	op = '*'
-	lbp = 60
+class Mul(Expr):
 	fields = 'left', 'right'
 
-class Div(BinaryOp):
-	op = '/'
-	lbp = 60
+class Div(Expr):
 	fields = 'left', 'right'
 
-class Mod(BinaryOp):
-	op = '%'
-	lbp = 55
+class Mod(Expr):
 	fields = 'left', 'right'
 
-class BWAnd(BinaryOp):
-	op = '&'
-	lbp = 40
+class BWAnd(Expr):
 	fields = 'left', 'right'
 
-class BWOr(BinaryOp):
-	op = '|'
-	lbp = 41
+class BWOr(Expr):
 	fields = 'left', 'right'
 
-class BWXor(BinaryOp):
-	op = '^'
-	lbp = 42
+class BWXor(Expr):
 	fields = 'left', 'right'
 
 class Not(Expr):
-	op = 'not'
-	lbp = 0
 	fields = 'value',
-	def nud(self, p):
-		self.value = p.expr()
-		return self
 
-class Owner(Node):
-	op = '$'
-	lbp = 0
+class Owner(Expr):
 	fields = 'value'
-	def nud(self, p):
-		self.value = p.expr(mode='type')
-		return self
 
-class Ref(Node):
-	op = '&'
-	lbp = 0
+class Ref(Expr):
 	fields = 'value'
-	def nud(self, p):
-		self.value = p.expr(mode='type')
-		return self
 
 class In(Expr):
-	op = 'in'
-	lbp = 70
-	fields = 'left', 'right'
-	def nud(self, p):
-		return self
-
-class And(BinaryOp):
-	op = 'and'
-	lbp = 15
 	fields = 'left', 'right'
 
-class Or(BinaryOp):
-	op = 'or'
-	lbp = 15
+class And(Expr):
 	fields = 'left', 'right'
 
-class EQ(BinaryOp):
-	op = '=='
-	lbp = 20
+class Or(Expr):
 	fields = 'left', 'right'
 
-class NE(BinaryOp):
-	op = '!='
-	lbp = 20
+class EQ(Expr):
 	fields = 'left', 'right'
 
-class LT(BinaryOp):
-	op = '<'
-	lbp = 20
+class NE(Expr):
 	fields = 'left', 'right'
 
-class GT(BinaryOp):
-	op = '>'
-	lbp = 20
+class LT(Expr):
 	fields = 'left', 'right'
 
-class As(BinaryOp):
-	
-	op = 'as'
-	lbp = 30
+class GT(Expr):
 	fields = 'left', 'right'
-		
-	def led(self, p, left):
-		self.left = left
-		self.right = p.expr(self.lbp, mode='type')
-		return self
 
-class Call(BinaryOp):
-	
-	op = '('
-	lbp = 70
-	fields = ('args',)
-	
-	def led(self, p, left):
-		
-		self.name = left
-		self.callbr = None
-		self.fun = None # for type inferencing
-		self.virtual = None # for type inferencing
-		self.args = []
-		if isinstance(p.token, RightPar):
-			p.advance(RightPar)
-			return self
-		
-		next = p.expr()
-		while isinstance(p.token, Comma):
-			self.args.append(next)
-			p.advance(Comma)
-			next = p.expr()
-		
-		self.args.append(next)
-		p.advance(RightPar)
-		return self
-	
-	def nud(self, p):
-		expr = p.expr()
-		p.token = p.next()
-		return expr
+class As(Expr):
+	fields = 'left', 'right'
+
+class Call(Expr):
+	fields = 'args',
 
 # Statement-level
 
-class Statement(Node):
-	lbp = 0
-
-class Assign(Statement):
-	
-	op = '='
-	lbp = 5
+class Assign(Node):
 	fields = 'left', 'right'
-	
-	def led(self, p, left):
-		self.left = left
-		self.right = p.expr(self.lbp)
-		return self
 
-class Raise(Statement):
-	
-	kw = 'raise'
-	lbp = 0
+class Raise(Node):
 	fields = 'value',
-	
-	def nud(self, p):
-		self.value = p.expr()
-		return self
 
-class Yield(Statement):
-	
-	kw = 'yield'
-	lbp = 0
+class Yield(Node):
 	fields = 'value',
-	
-	def nud(self, p):
-		self.value = p.expr()
-		self.target = None # for use in blocks
-		return self
 
-class Except(Statement):
-	
-	kw = 'except'
-	lbp = 0
+class Except(Node):
 	fields = 'type', 'suite'
-	
-	def nud(self, p):
-		self.type = p.expr()
-		p.eat(Colon)
-		self.suite = Suite(self.pos)
-		self.suite.eat(p)
-		return self
 
-class Suite(Statement):
-	
+class Suite(Node):
 	fields = 'stmts',
-	
-	def __init__(self, pos):
-		Node.__init__(self, pos)
-		self.stmts = []
-	
-	def eat(self, p):
-		
-		p.eat(NL)
-		p.advance(Indent)
-		p.eat(NL)
-		
-		while True:
-			self.stmts.append(p.expr())
-			p.eat(NL)
-			if isinstance(p.token, Dedent):
-				break
-		
-		p.advance(Dedent)
 
 class Argument(Node):
 	fields = 'name',
 	def __init__(self, pos):
 		Node.__init__(self, pos)
-		self.name = None
 		self.type = None
 
 class Decl(Node):
 	fields = 'decor', 'name', 'args', 'rtype'
 
 class TryBlock(Node):
-	
-	kw = 'try'
-	lbp = 0
 	fields = 'suite', 'catch'
-	
-	def nud(self, p):
-		
-		p.advance(Colon)
-		self.suite = Suite(self.pos)
-		self.suite.eat(p)
-		self.catch = []
-		while isinstance(p.token, Except):
-			self.catch.append(p.expr())
-		
-		assert len(self.catch) > 0, 'must have 1 or more except handlers'
-		return self
 
 class Function(Node):
-	
-	kw = 'def'
-	lbp = 0
 	fields = 'decor', 'name', 'args', 'rtype', 'suite'
-	
-	def nud(self, p):
-		
-		self.decor = set()
-		self.name = p.advance(Name)
-		p.advance(Call)
-		
-		self.args = []
-		cur = Argument(self.pos)
-		next = p.expr()
-		cur.pos = next.pos
-		while not isinstance(next, RightPar):
-			
-			cur.name = next
-			if isinstance(p.token, Colon):
-				p.advance(Colon, mode='type')
-				cur.type = p.expr(mode='type')
-			
-			self.args.append(cur)
-			cur	= Argument(self.pos)
-			next = p.expr()
-			
-			if isinstance(next, Comma):
-				next = p.expr()
-			
-			cur.pos = next.pos
-		
-		self.rtype = None
-		if isinstance(p.token, RType):
-			p.advance(RType)
-			self.rtype = p.expr()
-		
-		if not isinstance(p.token, Colon):
-			
-			p.advance(NL)
-			if isinstance(p.token, Indent):
-				raise util.Error(p.token, "no ':' after function header")
-			
-			decl = Decl(self.pos)
-			decl.decor = self.decor
-			decl.name = self.name
-			decl.args = self.args
-			decl.rtype = self.rtype
-			return decl
-		
-		p.advance(Colon)
-		self.suite = Suite(self.pos)
-		self.suite.eat(p)
-		return self
 
-class Pass(Statement):
-	
-	kw = 'pass'
-	lbp = 0
+class Pass(Node):
 	fields = ()
-	
-	def nud(self, p):
-		return self
 
-class Return(Statement):
-	
-	kw = 'return'
-	lbp = 0
+class Return(Node):
 	fields = 'value',
-	
-	def nud(self, p):
-		self.value = None
-		if not isinstance(p.token, NL):
-			self.value = p.expr()
-		return self
 
 class Ternary(Expr):
-	
-	lbp = 10
 	fields = 'cond', 'values'
-	
-	def __init__(self, pos):
-		Expr.__init__(self, pos)
-		self.cond = None
-		self.values = []
-	
-	def set(self, left, p):
-		self.values.append(left)
-		self.cond = p.expr()
-		p.advance(Else)
-		self.values.append(p.expr())
 
-class If(Statement):
-	
-	kw = 'if'
-	lbp = 10
+class If(Node):
 	fields = 'blocks',
-	
-	def led(self, p, left):
-		res = Ternary(self.pos)
-		res.set(left, p)
-		return res
-	
-	def nud(self, p):
-		
-		cond = p.expr()
-		p.advance(Colon)
-		block = Suite(self.pos)
-		block.eat(p)
-		self.blocks = [(cond, block)]
-		
-		while isinstance(p.token, Elif):
-			kw = p.advance(Elif)
-			cond = p.expr()
-			p.advance(Colon)
-			block = Suite(kw.pos)
-			block.eat(p)
-			self.blocks.append((cond, block))
-		
-		if isinstance(p.token, Else):
-			kw = p.advance(Else)
-			p.advance(Colon)
-			block = Suite(kw.pos)
-			block.eat(p)
-			self.blocks.append((None, block))
-		
-		return self
 
-class Elif(Node):
-	kw = 'elif'
-	lbp = 0
-	def nud(self, p):
-		return self
-
-class Else(Node):
-	kw = 'else'
-	lbp = 0
-	def nud(self, p):
-		return self
-
-class Import(Statement):
-	kw = 'import'
+class Import(Node):
 	fields = 'names',
-	def nud(self, p):
-		self.names = []
-		while isinstance(p.token, Name):
-			self.names.append(p.expr())
-			if isinstance(p.token, Comma):
-				p.advance(Comma)
-		return self
 
-class RelImport(Statement):
-	kw = 'from'
+class RelImport(Node):
 	fields = 'base', 'names'
-	def nud(self, p):
-		self.base = p.expr()
-		self.names = []
-		p.advance(Import)
-		while isinstance(p.token, Name):
-			self.names.append(p.expr())
-			if isinstance(p.token, Comma):
-				p.advance(Comma)
-		return self
 
-class For(Statement):
-	kw = 'for'
+class For(Node):
 	fields = 'lvar', 'source', 'suite'
-	def nud(self, p):
-		self.lvar = p.advance(Name)
-		p.advance(In)
-		self.source = p.expr()
-		p.advance(Colon)
-		self.suite = Suite(self.pos)
-		self.suite.eat(p)
-		return self
 
-class While(Statement):
-	kw = 'while'
+class While(Node):
 	fields = 'cond', 'suite'
-	def nud(self, p):
-		self.cond = p.expr()
-		p.advance(Colon)
-		self.suite = Suite(self.pos)
-		self.suite.eat(p)
-		return self
 
-class Decorator(Node):
-	
-	lbp = 0
-	
-	def __init__(self, val, pos):
-		Node.__init__(self, pos)
-		self.decor = {val[1:]}
-	
-	def nud(self, p):
-		p.eat(NL)
-		obj = p.expr()
-		assert hasattr(obj, 'decor')
-		obj.decor |= self.decor
-		return obj
-
-class Class(Statement):
-	
-	kw = 'class'
+class Class(Node):
 	fields = 'decor', 'name', 'params', 'attribs', 'methods'
-	
-	def nud(self, p):
-		
-		self.decor = set()
-		self.name = p.advance(Name)
-		self.params = []
-		
-		if isinstance(p.token, Elem):
-			p.advance(Elem)
-			self.params.append(p.advance(Name))
-			while not isinstance(p.token, ElemEnd):
-				p.advance(Comma)
-				self.params.append(p.advance(Name))
-			p.advance(ElemEnd)
-		
-		p.advance(Colon)
-		p.eat(NL)
-		p.advance(Indent)
-		p.eat(NL)
-		
-		self.attribs = []
-		self.methods = []
-		if isinstance(p.token, Pass):
-			p.advance(Pass)
-			p.eat(NL)
-			p.advance(Dedent)
-			return self
-		
-		while isinstance(p.token, Name):
-			field = p.expr()
-			p.advance(Colon, mode='type')
-			type = p.expr(mode='type')
-			self.attribs.append((type, field))
-			p.eat(NL)
-		
-		while isinstance(p.token, Function):
-			self.methods.append(p.expr())
-			p.eat(NL)
-		
-		p.advance(Dedent)
-		return self
 
-class Trait(Statement):
-	
-	kw = 'trait'
+class Trait(Node):
 	fields = 'decor', 'name', 'params', 'methods'
-	
-	def nud(self, p):
-		
-		self.decor = set()
-		self.name = p.advance(Name)
-		self.params = []
-		
-		if isinstance(p.token, Elem):
-			p.advance(Elem)
-			self.params.append(p.advance(Name))
-			while not isinstance(p.token, ElemEnd):
-				p.advance(Comma)
-				self.params.append(p.advance(Name))
-			p.advance(ElemEnd)
-		
-		p.advance(Colon)
-		p.eat(NL)
-		p.advance(Indent)
-		p.eat(NL)
-		
-		self.methods = []
-		while isinstance(p.token, Function):
-			self.methods.append(p.expr())
-			p.eat(NL)
-		
-		p.advance(Dedent)
-		return self
-
-# The core of the parsing algorithm
-
-OPERATORS = {cls.op: cls for cls in Registry.types if hasattr(cls, 'op')}
-KEYWORDS = {cls.kw: cls for cls in Registry.types if hasattr(cls, 'kw')}
-
-class Pratt(object):
-	
-	def __init__(self, tokens):
-		self.next = self.wrap(tokens).next
-		self.token = self.next()
-	
-	def wrap(self, tokens):
-		for t, v, s, e, l in tokens:
-			if t == 'name' and v in {'True', 'False'}:
-				yield Bool(v, (s, e, l))
-			elif t == 'name':
-				yield Name(v, (s, e, l))
-			elif t == 'num' and '.' not in v:
-				yield Int(v, (s, e, l))
-			elif t == 'num' and '.' in v:
-				yield Float(v, (s, e, l))
-			elif t == 'kw':
-				yield KEYWORDS[v]((s, e, l))
-			elif t == 'str':
-				yield String(v, (s, e, l))
-			elif t == 'op':
-				yield OPERATORS[v]((s, e, l))
-			elif t == 'indent' and v > 0:
-				yield Indent((s, e, l))
-			elif t == 'indent' and v < 0:
-				yield Dedent((s, e, l))
-			elif t == 'nl':
-				yield NL((s, e, l))
-			elif t == 'com':
-				continue
-			elif t == 'deco':
-				yield Decorator(v, (s, e, l))
-		yield End((s, e))
-	
-	def mode(self, t):
-		if t == 'type':
-			OPERATORS['&'] = Ref
-		elif t == 'expr':
-			OPERATORS['&'] = BWAnd
-		else:
-			assert False, t
-	
-	def eat(self, type):
-		while isinstance(self.token, type):
-			self.token = self.next()
-	
-	def advance(self, id, mode='expr'):
-		if not isinstance(self.token, id):
-			bits = id.__name__, self.token.__class__.__name__
-			raise util.Error(self.token, 'expected %r, got %r' % bits)
-		t = self.token
-		self.mode(mode)
-		self.token = self.next()
-		return t
-	
-	def expr(self, rbp=0, mode='expr'):
-		self.mode(mode)
-		t, self.token = self.token, self.next()
-		left = t.nud(self)
-		while rbp < self.token.lbp and not isinstance(left, Statement):
-			t, self.token = self.token, self.next()
-			left = t.led(self, left)
-		return left
 
 class Module(Node):
 	fields = 'suite',
 	def __init__(self):
 		self.suite = []
-
-def parse(tokens):
-	mod = Module()
-	p = Pratt(tokens)
-	p.eat(NL)
-	while not isinstance(p.token, End):
-		mod.suite.append(p.expr())
-		p.eat(NL)
-	return mod
