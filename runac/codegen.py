def2a632a2e2334a9fc4b593f027c061acd17997
Dirkjan Ochtman <dirkjan@ochtman.nl>
2014-10-23 21:57:36 +0200

    Upgrade OS X triple to 10.10

--- runac/codegen.py
+++ runac/codegen.py
@@ -1096,7 +1096,7 @@ class CodeGen(object):
 			self.visit(v, frame)
 
 TRIPLES = {
-	('64bit', 'darwin'): 'x86_64-apple-macosx10.9.0',
+	('64bit', 'darwin'): 'x86_64-apple-macosx10.10.0',
 	('64bit', 'linux2'): 'x86_64-pc-linux-gnu',
 	('32bit', 'linux2'): 'i386-pc-linux-gnu',
 }
